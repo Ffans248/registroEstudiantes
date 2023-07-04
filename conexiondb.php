d2e9c8cc6b5dcf045590669420c98580debdf4f2
Ffans <diegoescobar.estudios@gmail.com>
2023-07-03 22:26:25 -0600

    se adaptó la conexión de la base de datos a la de la web

--- conexiondb.php
+++ conexiondb.php
@@ -1,9 +1,9 @@
 <?php
 //CONEXION
 $server = "localhost";
-$user = "root";
-$pass = "";
-$db = "estudiantesregistro";
+$user = "id20936459_fafacompudb";
+$pass = "123UTMpassdb!";
+$db = "id20936459_estudiantesregistro";
 $conn = mysqli_connect($server, $user, $pass, $db);
 
 ?>
